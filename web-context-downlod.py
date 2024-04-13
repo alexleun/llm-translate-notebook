9d5038bb226b8284dc15f14ac8d5c815e8b02b25
alexleun <149701409+alexleun@users.noreply.github.com>
2024-04-13 12:12:44 +0800

    Update web-context-downlod.py
    debug for support more then 100 links.

--- web-context-downlod.py
+++ web-context-downlod.py
@@ -1,4 +1,3 @@
-# Get the web contain in Japan novel, work with jp site
 import requests
 from bs4 import BeautifulSoup
 import sys
@@ -10,32 +9,43 @@ def find_between(s, start, end):
 base_url = sys.argv[1]
 novel_num = sys.argv[2]
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-response = requests.get(base_url+novel_num+'/', headers=headers)
-soup = BeautifulSoup(response.content, "html.parser")
-novel_title = find_between(str(soup.find(class_="novel_title")), ">", "<")
-index_box = soup.find(class_="index_box")
-urls = index_box.find_all(class_="subtitle")
-sublist = []
-for url in urls:
-    ut = find_between(str(url),'/">','</a>')
-    ul = base_url+find_between(str(url),'href="/','">')
-    sublist.append([ut, ul])
 
-txt=novel_title+'\n'
-e = 0
-for subl in sublist:
-    print(subl[1])
-    e += 1
-    txt=txt+'Chapter '+str(e)+': '+subl[0]+'\n'
-    response = requests.get(subl[1], headers=headers)
+page_num = 1
+e = 0  # Initialize the variable 'e' outside the loop
+txt = ""  # Initialize the variable 'txt' outside the loop
+while True:
+    response = requests.get(base_url + novel_num + '/?p=' + str(page_num), headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
-    a = soup.find_all(class_="novel_view")
-    for b in a:
-        c=b.get_text(separator = '</p>').replace('</p>','')
-        txt=txt+c+'\n'
 
-# write to text file.
-text_file = open(novel_num+".txt", "w", encoding='UTF-8')
+    # Check if there are any URLs on the current page
+    index_box = soup.find(class_="index_box")
+    try:
+        urls = index_box.find_all(class_="subtitle")
+        if not urls:
+            break
+    except:
+        break
+
+    # Process the URLs on the current page
+    for url in urls:
+        ut = find_between(str(url), '/">', '</a>')
+        ul = base_url + find_between(str(url), 'href="/', '">')
+
+        print(ul)
+
+        e += 1
+        txt = txt + 'Chapter ' + str(e) + ': ' + ut + '\n'
+        response = requests.get(ul, headers=headers)
+        soup = BeautifulSoup(response.content, "html.parser")
+        a = soup.find_all(class_="novel_view")
+        for b in a:
+            c = b.get_text(separator='</p>').replace('</p>', '')
+            txt = txt + c + '\n'
+
+    page_num += 1
+
+# Write to text file.
+text_file = open(novel_num + ".txt", "a", encoding='UTF-8')
 if text_file.write(txt) != len(txt):
     print("Failure! String not written to text file.")
 text_file.close()
