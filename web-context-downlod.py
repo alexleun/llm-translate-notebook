e2537789fe65852ab635c34016c5b4d361453416
alexleun <149701409+alexleun@users.noreply.github.com>
2024-04-13 10:53:42 +0800

    Update web-context-downlod.py
    debug and command line parameter supported.

--- web-context-downlod.py
+++ web-context-downlod.py
@@ -1,13 +1,14 @@
 # Get the web contain in Japan novel, work with jp site
 import requests
 from bs4 import BeautifulSoup
+import sys
 
 def find_between(s, start, end):
     return s.split(start)[1].split(end)[0]
 
 # Get subtitle and url from source_url
-novel_num = "n0174gy"
-base_url = "https://ncode.syosetu.com/"
+base_url = sys.argv[1]
+novel_num = sys.argv[2]
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 response = requests.get(base_url+novel_num+'/', headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
@@ -31,7 +32,7 @@ for subl in sublist:
     a = soup.find_all(class_="novel_view")
     for b in a:
         c=b.get_text(separator = '</p>').replace('</p>','')
-    txt=txt+c+'\n'
+        txt=txt+c+'\n'
 
 # write to text file.
 text_file = open(novel_num+".txt", "w", encoding='UTF-8')
